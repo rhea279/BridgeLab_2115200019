e80c8da259d1b24d8d5ed155e1cf20a214ea828f
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-01-23 23:39:46 +0530

    Add Practice Questions Done on 19-01-2025

---
+++ 19-01-2025/Program02/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program02{
+	static void Main(string[] args){
+		string input1 = Console.ReadLine();
+		string input2 = Console.ReadLine();
+    	int n1 = int.Parse(input1);
+    	int n2 = int.Parse(input2);
+		int sum = n1+n2;
+		Console.WriteLine($"The sum of {n1} and {n2} is {sum}");
+	}
+}
---
+++ 19-01-2025/Program5/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program5{
+	static void Main(string[] args){
+    	Console.Write("Enter distance in kilometers:");
+    	string kilo = Console.ReadLine();
+        	double kilometers = double.Parse(kilo);
+    	double miles = kilometers*0.621371;
+    	Console.WriteLine($"{kilometers} Kms is equal to {miles}miles.");
+	}
+}
