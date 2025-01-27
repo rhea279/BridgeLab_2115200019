b6a31e1428ef71747ff3e0f24b3932d58a8b48cd
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-01-27 15:09:11 +0530

    Add Programming Element assignments

---
+++ 22-01-2025/ProgrammingElements Level2/Q5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class TemperatureConversion {
+	static void Main(string[] args){
+		//Input temperature in Fahrenheit from the User
+		Console.Write("Enter the temperature in Fahrenheit:");
+		double fahrenheit = Double.Parse(Console.ReadLine());
+		Console.Write($"The {fahrenheit} Fahrenheit is {TempConversion(fahrenheit):F2} Celsius");
+		
+	}
+	//Convert temperature from Fahrenheit to Celsius
+	static double TempConversion(double fahrenheit){
+		double cel = (fahrenheit - 32) * 5/9;
+		return cel;
+	}
+}
---
+++ 22-01-2025/ProgrammingElements Level2/Q9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class NumberOfRounds{
+	static void Main(string[] args){
+		//Input the sides of the triangle from user
+		Console.Write("Enter the sides of the triangle in Km:");
+		double s1 = Double.Parse(Console.ReadLine());
+		double s2 = Double.Parse(Console.ReadLine());
+		double s3 = Double.Parse(Console.ReadLine());
+		
+		double perimeter = s1+s2+s3;
+		double rounds = 5.0/perimeter;
+		
+		Console.Write($"The total number of rounds the athlete will run is {rounds} to complete 5 km");
+		
+	}
+}
