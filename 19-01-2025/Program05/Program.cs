f9e28a609c27e58aa66b5f9ca3838d264cfdcd1a
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-01-27 15:01:33 +0530

    Add Practice Assignment

---
+++ 19-01-2025/Program05/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program05{
+	static void Main(string[] args){
+		string r= Console.ReadLine();
+		string h = Console.ReadLine();
+		double radius = double.Parse(r); 
+		double height = double.Parse(h);
+		double volume= Math.PI * Math.Pow(radius, 2)*height; 
+		Console.WriteLine($"The volume of cylinder is: {volume}");
+	}
+}
